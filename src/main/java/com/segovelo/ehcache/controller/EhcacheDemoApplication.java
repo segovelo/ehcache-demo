07685b0700146cc61fca67c255005b60714a3e14
segovelo <gauchosinpingo@gmail.com>
2022-04-22 20:05:22 +0100

    Initial commit

---
+++ src/main/java/com/segovelo/ehcache/controller/EhcacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.segovelo.ehcache.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheDemoApplication.class, args);
+	}
+
+}
