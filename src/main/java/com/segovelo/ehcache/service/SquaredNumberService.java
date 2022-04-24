35f9a6531256204515df6e72b1b2fe3e11967de9
segovelo <gauchosinpingo@gmail.com>
2022-04-24 15:57:08 +0100

    Adding files to include Rest controller and endpoint get square number.

---
+++ src/main/java/com/segovelo/ehcache/service/SquaredNumberService.java
@@ -0,0 +1,23 @@
+package com.segovelo.ehcache.service;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SquaredNumberService {
+
+    private final static Logger log = LoggerFactory.getLogger(SquaredNumberService.class);
+
+    @Cacheable(value = "squareCache", key = "#number", condition = "#number>10")
+    public BigDecimal square(Long number) {
+        BigDecimal square = BigDecimal.valueOf(number)
+            .multiply(BigDecimal.valueOf(number));
+        log.info("square of {} is {}", number, square);
+        return square;
+    }
+
+}
