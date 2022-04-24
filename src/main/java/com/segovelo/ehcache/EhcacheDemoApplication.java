8448ff2d776d0020a488af97acf575ac763db010
segovelo <gauchosinpingo@gmail.com>
2022-04-24 15:56:07 +0100

    Moving EhcacheDemoApplication to parent package.

--- src/main/java/com/segovelo/ehcache/EhcacheDemoApplication.java
+++ src/main/java/com/segovelo/ehcache/EhcacheDemoApplication.java
@@ -1,4 +1,4 @@
-package com.segovelo.ehcache.controller;
+package com.segovelo.ehcache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
